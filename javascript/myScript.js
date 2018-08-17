1e0d13e11e6c6ccb683e08fb23fe8fd0e7a46fbd
andreyrehv <andreycaldas@gmail.com>
2018-08-16 22:33:17 -0300

    funcao js

--- javascript/myScript.js
+++ javascript/myScript.js
@@ -5,4 +5,11 @@ function myFunction() {
 
 function outrafuncao(){
 		console.log("vc so vai ver isso no console se chamar a funcao");
+		document.getElementById('demo').innerHTML = " Javascript rodou novamente?";
+}
+
+function myFunc(){
+	console.log("rodou sim ou nao?");
+	document.getElementById('demo2').innerHTML = " Consegui um ponto positivo? ";
+	alert('HELLO ITS ME');
 }
