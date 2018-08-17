7ed9f0bc890200f09d0e2ae5e04e738c91e46f7b
andreyrehv <andreycaldas@gmail.com>
2018-08-16 22:14:32 -0300

    aula de js

---
+++ javascript/myScript.js
@@ -0,0 +1,8 @@
+function myFunction() {
+	console.log("function myFunction() ...rodou");
+	document.getElementById('demo').innerHTML = "Javascript rodou sim";
+}
+
+function outrafuncao(){
+		console.log("vc so vai ver isso no console se chamar a funcao");
+}
