9746caefc60aec30a4f9b554cb13693da75815e1
andreyrehv <andreycaldas@gmail.com>
2018-08-30 20:29:30 -0300

    aula 6

---
+++ Aula6pw/aula6.js
@@ -0,0 +1,8 @@
+function multiplica(a,b){
+	console.log("function multiplica(a,b){...");
+	var resultado = a*b;
+	console.log("imprimindo resultado");
+	return a*b;
+	}
+	var resultadoDaFuncaoMultiplica = multiplica("azul","verde");
+	alert (resultadoDaFuncaoMultiplica);
