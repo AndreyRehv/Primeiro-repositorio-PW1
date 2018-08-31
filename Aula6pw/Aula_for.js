ea4cd21da89f98c87cdb0b76eddc0ddcd0eb00c3
andreyrehv <andreycaldas@gmail.com>
2018-08-30 22:27:34 -0300

    Aula de for

---
+++ Aula6pw/Aula_for.js
@@ -0,0 +1,13 @@
+function marcasdecarros(arrCarros){
+	var i;
+	for (i=0; i<= marcas.length; i++){
+		console.log(marcas[i]);
+	}
+}
+
+	var marcas=["FORD", "AUDI", "BMW", "FERRARI", "HONDA", "MERCEDES", "CHEVROLET", "McLAREN"]
+	
+	marcasdecarros(marcas);
+	
+	
+	
