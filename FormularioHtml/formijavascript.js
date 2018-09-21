057ab6dc9eb2b3247ffb489a0d3d823d9e05de72
andreyrehv <andreycaldas@gmail.com>
2018-09-20 21:35:30 -0300

    mb

--- FormularioHtml/formijavascript.js
+++ FormularioHtml/formijavascript.js
@@ -1,15 +1,45 @@
 function validaformulario(){
+    
+    // var nome= `document.forms["t1"]["cpf"].value`;
+    // if(nome == ""){
+    // }
+    
+    // var cpf=document.forms["t1"]["cpf"].value;
+    // if(cpf == "") {
+    //     alert("Esse campo não pode ficar em branco");
+    // }
 	
-	var nome="document.forms["t1"]["cpf"].value;
-	if(nome == ""){
-	}
 	
-	var cpf=document.forms["t1"]["cpf"].value;
-	if(cpf == "") {
-		alert("Esse campo não pode ficar em branco");
-	}
+    const form = document.getElementsByName("t1")[0].getElementsByTagName('input') 
+    var res = '' 
 
+    for (let i = 0; i < form.length; i++) { 
+        if (form[i].value == '') res +=  form[i].name + '\n'  
+		}
+    
+    if (res != '') alert('O(s) campo(s) precisa(m) ser preenchido(s): ' + '\n' + res + '') 
+    
+	
+	var cpf=t1.cpf.value;
+	if(cpf.length != 11){
+		alert('INVALIDO')
+		t1.cpf.focus();
+		    return false;
+	}
+	
+	var cep=t1.cep.value;
+	if(cep.length != 8){
+		alert('INVALIDO')
+		t1.cep.focus();
+	
+	return false;
+	}
 }
 
+const form2 = document.getElementsByTagName("form")
+for(let i = 0; i<form2.length; i++){
+    form2[i].getElementsByTagName('input')
+}
+console.log('Inputs dos formulários', form2)
 
 
